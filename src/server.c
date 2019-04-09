25ee8c37d7397db0b29f986b9a3ecf61d519202d
jeffyjkang <jeffyjkang@gmail.com>
2019-04-08 22:39:10 -0700

    not printing randnum

--- src/server.c
+++ src/server.c
@@ -92,13 +92,16 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
 void get_d20(int fd)
 {
     // Generate a random number between 1 and 20 inclusive
-
+    char body[20];
+    int rand_num = rand() % 20 + 1;
+    sprintf(body, "%d", rand_num);
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
 
     // Use send_response() to send it back as text/plain data
-
+    // expects fd, header, content type, body, content length
+    send_response(fd, "HTTP/1.1 200 OK", "text/plain", body, strlen(body));
     ///////////////////
     // IMPLEMENT ME! //
     ///////////////////
@@ -175,13 +178,41 @@ void handle_http_request(int fd, struct cache *cache)
     // IMPLEMENT ME! //
     ///////////////////
 
+    // printf("%s\n", request);
+    // buffer to hold the method
+    char method[200];
+    // buffer to hold the path
+    char path[8192];
+
     // Read the three components of the first request line
+    // expects const char *str, const char format
+    sscanf(request, "%s %s", method, path);
+    printf("method: %s\npath: %s\n", method, path);
 
     // If GET, handle the get endpoints
-
-    //    Check if it's /d20 and handle that special case
-    //    Otherwise serve the requested file by calling get_file()
-
+    if (strcmp(method, "GET") == 0)
+    {
+        //    Check if it's /d20 and handle that special case
+        // check to see if path is /d20
+        if (strcmp(path, "/d20") == 0)
+        {
+            // handle special case
+            get_d20(fd);
+        }
+        // otherwise serve requested file
+        else
+        {
+            //    Otherwise serve the requested file by calling get_file()
+            // call get file, expects fd, cache and
+            get_file(fd, cache, path);
+        }
+    }
+    // if method not get, this duplicates, but gives date, recheck
+    else
+    {
+        // call 404 page
+        resp_404(fd);
+    }
     // (Stretch) If POST, handle the post request
 }
 
@@ -234,7 +265,7 @@ int main(void)
         // listenfd is still listening for new connections.
 
         handle_http_request(newfd, cache);
-        resp_404(newfd);
+        // resp_404(newfd);
 
         close(newfd);
     }
